6ec32e70bd174ee9a20279fd5332da6b40493007
vkobinski <victorkobinski@outlook.com>
2023-11-11 01:12:48 -0300

    Pong

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::Write;
+
+use tokio::stream;
+
+fn handle_connection(mut stream : TcpStream) {
+
+    match stream.write_all(b"+PONG\r\n") {
+
+        Ok(()) => {
+
+        } Err(e) => {
+            println!("error: {}", e);
+
+        }
+    };
+}
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,8 +23,9 @@ fn main() {
 
      for stream in listener.incoming() {
          match stream {
-             Ok(_stream) => {
+             Ok(stream) => {
                  println!("accepted new connection");
+                 handle_connection(stream);
              }
              Err(e) => {
                  println!("error: {}", e);
