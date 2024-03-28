6cd0b52ddb33fb9d83dcd67b88e7df96beb62e0b
vkobinski <victorkobinski@outlook.com>
2024-03-28 19:57:19 -0300

    ack command

--- src/redis/handler.rs
+++ src/redis/handler.rs
@@ -203,6 +203,9 @@ pub fn handle_psync(persistence: &State, stream: &mut TcpStream, _vals: &[RespDa
 }
 
 pub fn handle_request(persistence: &State, stream: &mut TcpStream, req: &Resp) {
+
+    println!("Request: {:?}", req.data);
+
     match &req.data {
         RespData::Array(vals) => match vals.get(0).unwrap() {
             RespData::BulkString(command) => match command.to_lowercase().as_str() {
